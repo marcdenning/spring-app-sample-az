d598e368d47194ad92abbf07d1642860d3ace9fb
Marc Denning <marc.denning@gmail.com>
2021-01-29 16:06:11 -0500

    Add functional test for API endpoint and enable code coverage in pipeline.

--- src/test/java/com/marcdenning/azure/app/SpringAppSampleApplicationTests.java
+++ src/test/java/com/marcdenning/azure/app/SpringAppSampleApplicationTests.java
@@ -1,13 +1,47 @@
 package com.marcdenning.azure.app;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.MAP;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringAppSampleApplicationTests {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void helloWorldReturnsMessage() throws JsonProcessingException {
+        final ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:" + port + "/api/hello", String.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        final Map<String, String> bodyMap = objectMapper.readValue(responseEntity.getBody().toString(), new TypeReference<HashMap<String, String>>() {});
+
+        assertThat(bodyMap).containsKey("hello");
+        assertThat(bodyMap.get("hello")).isEqualTo("world");
     }
 
 }
